db3fbafc7914ad17552b94a579e69d195c8493fc
Jeyhun Abilov <abilovv599@gmail.com>
2023-09-05 22:44:51 +0400

    redirecting fixed

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,10 +1,12 @@
 import thunk from "redux-thunk";
 import userFeedSlice from "../slice/userFeedSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import tokenSlice from "../slice/tokenSlice";
 
 const store = configureStore({
   reducer: {
     userFeed: userFeedSlice,
+    token: tokenSlice,
   },
   middleware: [thunk],
 });
